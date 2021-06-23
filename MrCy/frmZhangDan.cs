f6e6c5d1e35a336f22fdb66397904484d60c7c7d
刘展鹏 <1828006237@qq.com>
2021-06-23 10:20:10 +0800

    更改了excel有关的地方（会报错的地方——已解决）

--- MrCy/frmZhangDan.cs
+++ MrCy/frmZhangDan.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace MrCy
 {
@@ -24,14 +25,14 @@ namespace MrCy
             string strCon = "server=(local);database=db_MrCy;Integrated security=true";
             SqlConnection conn = new SqlConnection(strCon);
             conn.Open();
-       
-            SqlDataAdapter da = new SqlDataAdapter("select guestname,roomname, sum(price)as allprice,datatime,waitername from tb_zhangdan group by guestname,waitername,roomname,datatime having datatime ='"+str+"'", conn);
+
+            SqlDataAdapter da = new SqlDataAdapter("select guestname,roomname, sum(price)as allprice,datatime,waitername from tb_zhangdan group by guestname,waitername,roomname,datatime having datatime ='" + str + "'", conn);
             DataSet ds = new DataSet();
             da.Fill(ds);
             dataGridView1.DataSource = ds.Tables[0];
             conn.Close();
 
-           
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -41,11 +42,11 @@ namespace MrCy
 
 
         #region 导出gridview中的数据
-       
+
         private void button2_Click(object sender, EventArgs e)
         {
 
-            
+
             int x = dataGridView1.Rows.Count;
             int y = dataGridView1.Columns.Count;
             int i = 1, j = 1;
@@ -53,42 +54,42 @@ namespace MrCy
             Excel.Application excle = new Excel.Application();
             excle.Application.Workbooks.Add(true);
             excle.Visible = true;
-            try 
-            
-            {
-            
-          
-    
-          for (i = 1; i <=x+1; i++)
+            try
+
             {
 
-                 for (j = 1; j <= y ; j++)
 
+
+                for (i = 1; i <= x + 1; i++)
                 {
-                
-                    if (i == 1)
+
+                    for (j = 1; j <= y; j++)
+
                     {
-                        excle.Cells[i, j] = dataGridView1.Columns[j-1].HeaderText;
+
+                        if (i == 1)
+                        {
+                            excle.Cells[i, j] = dataGridView1.Columns[j - 1].HeaderText;
+                        }
+
+                        else
+                        {
+                            excle.Cells[i, j] = dataGridView1.Rows[i - 2].Cells[j - 1].Value.ToString();
+                        }
                     }
 
-                    else
-                    {
-                        excle.Cells[i, j] = dataGridView1.Rows[i-2].Cells[j-1].Value.ToString();
-                    }
+
+
                 }
 
 
 
             }
-                 
-            
-            
-            }
-            catch(Exception)
+            catch (Exception)
             {
 
                 MessageBox.Show("导出失败");
-            
+
             }
 
 
